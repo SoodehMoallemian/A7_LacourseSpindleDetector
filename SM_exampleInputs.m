8638ae6d1108187edd8865c2facb5fa16dea657e
Soodeh Moallemian <sm3177@newark.rutgers.edu>
2024-06-04 13:53:24 -0700

    edited example input file

--- SM_exampleInputs.m
+++ SM_exampleInputs.m
@@ -1,4 +1,3 @@
-subject_id = 'COV207';
-mainpath = fullfile('D:\Studies\01_DREEM3\02_RawData\');
-dep_path = fullfile('D:\Git\A7_LacourseSpindleDetector\lib\');
-folderName = 'Spindles';
+sub_id = 'COV106';
+source_path = fullfile('D:\Studies\01_DREEM3\02_RawData\');
+dep_path =fullfile('D:\Git\A7_LacourseSpindleDetector\lib\');
